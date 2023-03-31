c98ff4d2eadbcdc2d5ffb13185703ef5c91ae3f9
Cameron Matherne <cameronpmatherne@gmail.com>
2023-03-31 14:16:38 -0500

    cleaning up components

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -11,12 +11,14 @@ import Title from "./components/Title";
 
 export default function RestaurantDetails() {
   return (
+
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
         <Header />
         {/* Restaurant Description  */}
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+
           <div className="bg-white w-[70%] rounded p-3 shadow">
             <RestaurantNavBar />
             <Title />
@@ -25,13 +27,12 @@ export default function RestaurantDetails() {
             <Images />
             <Reviews />
           </div>
-        </div>
 
-        {/* Reservation Card Sidebar */}
-        <div className="w-[27%] relative text-reg">
-          <ReservationCard />
+          <div className="w-[27%] relative text-reg">
+            <ReservationCard />
+          </div>
+          
         </div>
-
       </main>
     </main>
 
