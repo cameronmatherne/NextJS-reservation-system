f9ca73c9c9fd86b69e61740abf93fa972fc06aba
Cameron Matherne <cameronpmatherne@gmail.com>
2023-04-02 13:04:30 -0500

    fetched the data from seeded database

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,42 @@
-"use client";
-import styles from './page.module.css'
-import NavBar from "./components/NavBar";
+
 import Header from "./components/Header";
-import { useRouter } from "next/navigation";
-import React, { useState } from 'react';
 import RestaurantCard from './components/RestaurantCard';
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
 
 // CF79QTIHOIksirkq
 
-export default function Home() {
-  const router = useRouter();
-  const [location, setLocation] = useState("");
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine,
+  slug: string;
+  location: Location,
+  price: PRICE
+}
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      location: true,
+      price: true
+    }
+  });
+
+
+  return restaurants;
+}
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
+  console.log({ restaurants });
 
   return (
     <>
@@ -18,7 +44,9 @@ export default function Home() {
         <main>
           <Header />
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <RestaurantCard />
+            {restaurants.map((restaurant) => (
+              <RestaurantCard restaurant={restaurant}/>
+            ))}
           </div>
         </main>
       </main>
